a2ef08de2b847800d2d34e5af2f3190381fb5331
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-10 01:30:28 +0100

    add a button remove 1 with this.removeOne.bind in the component Welcome.js

--- first-project/src/component/Welcome.js
+++ first-project/src/component/Welcome.js
@@ -27,6 +27,8 @@ class Welcome extends Component{
             <h1>Welcome {this.props.name}</h1>
             <p>Mon compteur : {this.state.count}</p>
             <button onClick={() => this.addOne()}>Ajouter 1</button>
+            <button onClick={this.removeOne.bind(this)}>Enlever 1</button>
+
             </div>
         );
     }
