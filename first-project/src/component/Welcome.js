c77f2a10a4506e5463881954f91222804f6d3dd7
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-10 01:14:29 +0100

    add this.props.name in the component Welcome.js

--- first-project/src/component/Welcome.js
+++ first-project/src/component/Welcome.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Welcome extends Component{
     render(){
         return(
-            <h1>Welcome</h1>
+            <h1>Welcome {this.props.name}</h1>
         );
     }
 }
